713ad818fd39b249d9fbbcda2b906cbc00d10458
Anonymous <paramjeet@guise.ai>
2021-10-01 11:04:16 +0530

    .

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -1,5 +1,6 @@
+import BG1 from "../../assets/images/25332.jpg";
 import BG2 from "../../assets/images/15152.jpg";
-import BG3 from "../../assets/images/3400343.jpg";
+import BG3 from "../../assets/images/rm378-08.jpg";
 import { useEffect } from "react";
 
 export const Home = () => {
@@ -8,37 +9,44 @@ export const Home = () => {
     }, [])
 
     return (
-        <div className='pt-16'>
-            <div>
-                <div className="flex justify-center items-center h-screen"
-                    style={{
-                        background: `url(${BG3}) no-repeat center center`,
-                        backgroundSize: 'cover',
-                        boxShadow: 'inset 0 0 0 2000px rgba(8, 8, 8, 0.2)',
-                    }}>
-                    <div className="flex">
-                        <h1 className="text-lg md:text-4xl flex items-center justify-center font-black uppercase  text-center mx-20 md:mx-80">
-                            Our AI solutions on the edge deliver accurate insights helping you to make impactful business decisions
-                        </h1>
-                    </div>
-                </div>
 
-
-                <div className="h-screen flex flex-wrap content-center text-gray-200 ">
-                    <div className=" flex flex-col justify-center items-center font-bold md:mx-36 text-sm md:text-3xl space-y-20 uppercase text-left  ">
-                        <h1>Guise AI solves the cost and efficiency problems endemic in AI today. Our continuous learning AI solutions on the edge allow machines to continuously learn and adapt to dynamically changing data in the real world without having to constantly retrain models</h1>
-                        <h1> Clients and OEMs who require a fast, cost-efficient and secure solution turn to Guise AI to continuously extract patterns and predict from real-time and dynamically changing data to create the greatest impact on end-users</h1>
-                    </div>
-                </div>
-
-
-                <div className="h-screen flex justify-end items-center "
-                    style={{
-                        background: `url(${BG2}) no-repeat center center`,
-                        backgroundSize: 'cover',
-                    }}>
+        <>
+            <div className="h-screen flex flex-wrap content-center text-gray-200"
+                style={{
+                    background: `url(${BG3}) no-repeat center center`,
+                    backgroundSize: 'cover',
+                    // boxShadow: 'inset 0 0 0 2000px rgba(8, 8, 8, 0.1)',
+                }}>
+                <div className="flex items-center justify-center">
+                    <h1 className="text-base md:text-2xl sm:text-lg lg:text-5xl lg:tracking-normal  lg:leading-snug font-black uppercase text-center mx-20 lg:mx-36 md:mx-50 sm:mx-20">
+                        Our AI solutions on the edge deliver accurate insights helping you to make impactful business decisions
+                    </h1>
                 </div>
             </div>
-        </div>
+
+
+            <div className="h-screen flex flex-wrap content-center text-gray-800 "
+                style={{
+                    background: `url(${BG1}) no-repeat center center `,
+                    backgroundSize: 'cover',
+                    // boxShadow: 'inset 0 0 0 2000px #0808081f',   
+
+                }}>
+
+                <div className=" flex flex-col justify-center  items-center lg:leading-snug lg:tracking-normal  font-black sm:text-base md:text-2xl   lg:font-black md:mx-36 lg:mx-40 text-sm lg:text-4xl space-y-20  text-left  ">
+                    <h1>Guise AI solves the cost and efficiency problems endemic in AI today. Our continuous learning AI solutions on the edge allow machines to continuously learn and adapt to dynamically changing data in the real world without having to constantly retrain models</h1>
+                    <h1> Clients and OEMs who require a fast, cost-efficient and secure solution turn to Guise AI to continuously extract patterns and predict from real-time and dynamically changing data to create the greatest impact on end-users</h1>
+                </div>
+
+            </div>
+
+            <div className="h-screen flex justify-end items-center "
+                style={{
+                    background: `url(${BG2}) no-repeat center center`,
+                    backgroundSize: 'cover',
+                }}>
+            </div>
+        </>
+
     )
 }
